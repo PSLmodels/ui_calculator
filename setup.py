3b3b278f81fc1523c821ea45cea0748d52627fc2
Max Ghenis <mghenis@gmail.com>
2020-07-16 22:26:30 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='ui_calculator',
+      version='0.1.0',
+      description='Unemployment insurance benefit calculator',
+      url='http://github.com/ganong-noel/ui_calculator',
+      author='Peter Ganong, Pascal Noel, Peter Robertson and Joseph Vavra',
+      author_email='ganong@uchicago.edu',
+      license='GPL',
+      packages=['ui_calculator'],
+      install_requires=[
+          'numpy',
+          'pandas'
+      ],
+      zip_safe=False)
