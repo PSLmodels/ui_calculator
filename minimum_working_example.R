c5c30db616c592a499f54ecc21e91d9e88bba4da
Max Ghenis <mghenis@gmail.com>
2020-09-13 09:47:44 -0700

    Resolve merge conflicts in minimum_working_example.R

--- minimum_working_example.R
+++ minimum_working_example.R
@@ -15,7 +15,6 @@ ui_calculator = import("ui_calculator")
 income_data <- read.csv("example_annual.csv")
 
 income_data <- income_data %>% mutate(weekly_earnings = wage/weeks_worked,
-<<<<<<< HEAD
                                       q1_earnings = weeks_worked - 39,
                                       q2_earnings = weeks_worked - 26,
                                       q3_earnings = weeks_worked - 13,
@@ -30,19 +29,4 @@ income_data <- income_data %>% mutate(weekly_earnings = wage/weeks_worked,
                                               q3_earnings,
                                               q4_earnings,
                                               state) %>% map_dbl(1))
-=======
-                   q1_earnings = weeks_worked - 39,
-                   q2_earnings = weeks_worked - 26,
-                   q3_earnings = weeks_worked - 13,
-                   q4_earnings = weeks_worked) %>%
-  mutate_at(vars(matches("q[1-4]_earnings" )), ~ case_when(.x > 13 ~ 13*weekly_earnings,
-                                                           .x < 0 ~ 0,
-                                                           TRUE ~ .x*weekly_earnings)) %>%
-  mutate(benefits_amount = calc_weekly_state_quarterly(q1_earnings,
-                                                       q2_earnings,
-                                                       q3_earnings,
-                                                       q4_earnings,
-                                                       state,
-                                                       weeks_worked) %>% map_dbl(1))
->>>>>>> master
 print(income_data)
